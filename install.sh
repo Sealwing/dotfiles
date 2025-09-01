b0124f4a27bc1e61473c6d99f08194cbf4f6f8db
sealwing <sealinsky@gmail.com>
2025-09-01 11:15:31 +0500

    Unified install script

--- install.sh
+++ install.sh
@@ -1,11 +1,44 @@
-# ghostty
-rm $HOME/Library/Application\ Support/com.mitchellh.ghostty/config
-ln -s ~/dotfiles/ghostty/config $HOME/Library/Application\ Support/com.mitchellh.ghostty/config 
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    echo "Linux detected"
+    if [[ -z "$XDG_CONFIG_HOME" ]]; then
+        GHOSTTY_CONFIG="$HOME/ghostty/config"
+    else
+        GHOSTTY_CONFIG="$XDG_CONFIG_HOME/ghostty/config"
+    fi
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    echo "MacOS detected"
+    GHOSTTY_CONFIG="$HOME/Library/Application Support/com.mitchellh.ghostty/config"
+else
+    echo "Can't detect platfor / no settings for it"
+    echo "Exiting"
+    exit 1
+fi
 
-# neovim
-rm -r ~/.config/nvim
-ln -s ~/dotfiles/nvim-cfg ~/.config/nvim
+DOTFILES_DIR="$HOME/dotfiles"
+echo "DOTFILES detected in $DOTFILES_DIR"
 
-# zsh
-rm -r ~/.zshrc
-ln -s ~/dotfiles/zsh/.zshrc ~/.zshrc
+GHOSTTY_SOURCE="$DOTFILES_DIR/ghostty/config"
+NEOVIM_SOURCE="$DOTFILES_DIR/nvim-cfg"
+ZSH_SOURCE="$DOTFILES_DIR/zsh/.zshrc"
+TMUX_SOURCE="$DOTFILES_DIR/tmux/.tmux.conf"
+
+link_config () {
+    local NAME=$1
+    local SOURCE_CONFIG=$2
+    local TARGET_CONFIG=$3
+
+    echo "Configuring $NAME"
+
+    echo "Removing $TARGET_CONFIG"
+    rm -r "$TARGET_CONFIG"
+
+    echo "Linking configuration from $SOURCE_CONFIG to $TARGET_CONFIG"
+    ln -s "$SOURCE_CONFIG" "$TARGET_CONFIG"
+    echo "---------------------------------------------------------------"
+}
+
+echo "---------------------------------------------------------------"
+link_config ghostty "$GHOSTTY_SOURCE" "$GHOSTTY_CONFIG"
+link_config neovim "$NEOVIM_SOURCE" "$HOME/.config/nvim"
+link_config zsh "$ZSH_SOURCE" "$HOME/.zshrc"
+link_config tmux "$TMUX_SOURCE" "$HOME/.tmux.conf" 
